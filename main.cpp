ac9494be88aaa4f554fa2f7dafbe7416c0ada0a6
Minus-cell <2831303449@qq.com>
2026-04-27 18:31:24 +0800

    modify sort function

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
 
 void sort(int arr[], int size)
 {
 	int tmp = 0;
+	bool flag = false;
 	for (int i = 0;i < size - 1;i++)
 	{
 		for (int j = 0;j < size - i - 1;j++)
 		{
-			if (arr[j] > arr[j + 1])
+			if (arr[j] < arr[j + 1])
 			{
 				tmp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = tmp;
+				flag = true;
 			}
+		}
+		if (!flag)
+		{
+			break;
 		}
 	}
 }
